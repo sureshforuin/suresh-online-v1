5c63b087cdbbec17f2dc73f10cee0c762ff9081e
sureshforuin <sureshforuin@gmail.com>
2025-06-01 21:33:37 +0530

    fix

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -23,8 +23,9 @@ class Portfolio extends Component<PortfolioProps> {
             src={projects.url} 
             title={projects.title} 
             frameborder={0}
+            allowfullscreen={true}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+            referrerpolicy="strict-origin-when-cross-origin">
             </iframe>
         </div>
 
