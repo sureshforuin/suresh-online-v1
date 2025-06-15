f9fe095f41534896ca87300f830c65cfb1ba6477
sureshforuin <sureshforuin@gmail.com>
2025-06-15 18:44:02 +0530

    reverting changes in src/App.tsx and src/Components/Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -49,11 +49,11 @@ class Header extends Component<HeaderProps> {
               </a>
             </li>
 
-            <li>
+            {/* <li>
               <a className="smoothscroll" href="#contact">
                 Contact
               </a>
-            </li> 
+            </li> */}
           </ul>
         </nav>
 
